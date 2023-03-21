32158c1dd68d8b56d90ce4a6eed12681661f343c
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-03-21 15:13:13 -0700

    Add basic functions gen_private_key, connect, and disconnect

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use rand_core::OsRng;
+use x25519_dalek::StaticSecret;
+
+pub fn gen_private_key() -> [u8; 32] {
+    StaticSecret::new(OsRng).to_bytes()
+}
+
+pub fn connect(
+    _portal_url: String,
+    _token: String,
+    _private_key: [u8; 32],
+) -> Result<u32, &'static str> {
+    let fd: u32 = 0;
+    // TODO: Mock websocket connection, periodically returning updated resources to the client
+    Ok(fd)
+}
+
+pub fn disconnect() -> Result<bool, &'static str> {
+    Ok(true)
+}
