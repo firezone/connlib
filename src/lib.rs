6a2a9952e6b755a1e04fa9559c19d2b0d47add85
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-03-22 11:50:51 -0700

    Add Session struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,36 @@
-pub fn connect(_portal_url: String, _token: String) -> Result<u32, &'static str> {
-    let fd: u32 = 0;
-    // TODO: Mock websocket connection, periodically returning updated resources to the client
-    Ok(fd)
+/// Firezone explicitly ties the WireGuard tunnel interface lifetime to
+/// the Control place connection lifetime under a single struct called Session.
+pub struct Session {
+    fd: u32,
+    handle: u32,
 }
 
-pub fn disconnect() -> Result<bool, &'static str> {
+pub fn connect(_portal_url: String, _token: String) -> Result<Session, &'static str> {
+    let fd = 9999;
+    let handle = fd + 1;
+
+    // 1. Find a free file descriptor
+    // 2. Allocate a device handle
+    // 3. Save this mapping for looking up later
+    // 4. Connect the WebSocket
+    println!("Found a free file descriptor: {fd}");
+    println!("Allocated device handle: {handle}");
+
+    // TODO: Mock websocket connection, periodically returning updated resources to the client
+    println!("Connected to portal");
+
+    Ok(Session { fd, handle })
+}
+
+pub fn disconnect(session: Session) -> Result<bool, &'static str> {
+    // 1. Close the websocket connection
+    // 2. Free the device handle
+    // 3. Close the file descriptor
+    // 4. Remove the mapping
+    println!("Closed the websocket connection");
+    println!("Freed the device handle {}", session.handle);
+    println!("Closed the file descriptor {}", session.fd);
+    println!("Removed the mapping");
+
     Ok(true)
 }
