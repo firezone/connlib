e47e6c0c9bc793ae44c9e290b13bcdb6ad5f161a
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-04-03 14:56:51 -0500

    still trying to figure obscure git error

--- Package.swift
+++ Package.swift
@@ -5,12 +5,14 @@
 import PackageDescription
 
 let package = Package(
-  name: "ConnlibRoot",
+  name: "ConnlibKit",
   platforms: [
       .macOS(.v12),
       .iOS(.v15)
   ],
-  products: [],
+  products: [
+    .library(name: "ConnlibKit", targets: ["ConnlibKit"]),
+    ],
   dependencies: [
       .package(path: "./apple")
   ],
@@ -18,7 +20,7 @@ let package = Package(
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
-      name: "ConnlibRoot",
+      name: "ConnlibKit",
         dependencies: [
           .product(name: "Connlib", package: "Connlib")
         ])
--- apple/Package.swift
+++ apple/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
-      name: "connlib",
+      name: "Connlib",
       targets: ["Connlib"])
   ],
   dependencies: [],
