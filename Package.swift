7e24936f47cd507e2b42ca1c81b2ee0fa21e48e2
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-04-03 14:47:05 -0500

    remove product

--- Package.swift
+++ Package.swift
@@ -4,17 +4,12 @@
 import PackageDescription
 
 let package = Package(
-  name: "ConnlibKit",
+  name: "ConnlibRoot",
   platforms: [
       .macOS(.v12),
       .iOS(.v15)
   ],
-  products: [
-  // Products define the executables and libraries a package produces, and make them visible to other packages.
-  .library(
-      name: "connlib",
-      targets: ["ConnlibKit"])
-  ],
+  products: [],
   dependencies: [
       .package(path: "./apple")
   ],
@@ -22,9 +17,9 @@ let package = Package(
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
-      name: "ConnlibKit",
+      name: "ConnlibRoot",
         dependencies: [
-          .product(name: "Connlib", package: "apple")
+          .product(name: "Connlib", package: "Connlib")
         ])
   ]
 )
