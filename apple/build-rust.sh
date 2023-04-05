b2e0ba3806a501e852d879129f4a8e3b50e177a9
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-04-05 10:03:51 -0500

    Pass RUSTFLAGS properly

--- apple/build-rust.sh
+++ apple/build-rust.sh
@@ -16,8 +16,9 @@ cd $PROJECT_DIR
 export PATH="$HOME/.cargo/bin:$PATH"
 
 # Use llvm from homebrew if installed. Fixes https://github.com/briansmith/ring/issues/1374
-[ -d "/opt/homebrew/opt/llvm/lib/c++" ] && export LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
-[ -d "/opt/homebrew/opt/llvm/bin" ] && export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+[ -d "/opt/homebrew/opt/llvm" ] \
+  && export RUSTFLAGS="-C link-arg=-L/opt/homebrew/opt/llvm/lib/c++ -C link-arg=-Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++" \
+  && export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 
 # Without this we can't compile on MacOS Big Sur
 # https://github.com/TimNN/cargo-lipo/issues/41#issuecomment-774793892
